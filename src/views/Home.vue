ed7cbc6e8a5c2b5dfa44ce2e7eb25d2258747e32
Gideon-Daniels <gideondaniels.dragoonix@gmail.com>
2021-10-14 14:42:07 +0200

    post

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,28 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList v-if="showPosts" :posts="posts"/>
-    <button @click="showPosts = !showPosts">toggle posts</button>
-    <button @click="posts.pop()">delete a post</button>
+    <div v-if="error"> {{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts"/>
+    </div>
+    <div v-else>Loading ....</div>
   </div>
 </template>
 
 <script>
 import PostList from '../components/PostList.vue' 
-import { ref } from 'vue'
+import getPosts from '../composables/getPosts'
 
 export default {
   name: 'Home',
   components: { PostList },
   // beginning of composition API
-  setup() {
-    const posts = ref([
-      { title: 'welcome to the blog', body: '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero maiores repellendus nam rerum pariatur atque quaerat expedita accusantium nobis voluptatibus alias commodi porro excepturi quae, adipisci corrupti ad dolores voluptas.', id : 1},
-      { title: 'top 5 CSS tips', body: '  Lorem ipsum dolor .', id : 2}
-    ])
+  setup() {   
+    const { posts, error, load} = getPosts()
 
-    const showPosts = ref(true)
-
-    return { posts, showPosts }
+    load()
+    
+    return { posts, error }
   },
 
 }
