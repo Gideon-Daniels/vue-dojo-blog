d0142a84f6be0a690b33f051f6518637113b1784
Gideon-Daniels <gideondaniels.dragoonix@gmail.com>
2021-10-14 13:43:39 +0200

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <PostList v-if="showPosts" :posts="posts"/>
+    <button @click="showPosts = !showPosts">toggle posts</button>
+    <button @click="posts.pop()">delete a post</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import PostList from '../components/PostList.vue' 
+import { ref } from 'vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: { PostList },
+  // beginning of composition API
+  setup() {
+    const posts = ref([
+      { title: 'welcome to the blog', body: '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero maiores repellendus nam rerum pariatur atque quaerat expedita accusantium nobis voluptatibus alias commodi porro excepturi quae, adipisci corrupti ad dolores voluptas.', id : 1},
+      { title: 'top 5 CSS tips', body: '  Lorem ipsum dolor .', id : 2}
+    ])
+
+    const showPosts = ref(true)
+
+    return { posts, showPosts }
+  },
+
 }
 </script>
