718dc44b249a0d1487913b3a68c0020e1963ad11
Gideon-Daniels <gideondaniels.dragoonix@gmail.com>
2021-10-15 09:47:23 +0200

    update

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="create">
-      <form>
+      <form @submit="handleSubmit">
         <label>Title:</label>
         <input v-model="title" type="text" required>
         <label>Content:</label>
@@ -36,8 +36,22 @@ export default {
             }
             tag.value = ''
         }
+
+        // handle submit event
+        const handleSubmit = async () => {
+            const post = {
+                title: title.value,
+                body: body.value,
+                tags: tags.value
+            }
+            await fetch('http://localhost:3000/posts', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(post)         
+            })
+        }
     
-        return { title, body, tag, tags, handleKeyDown, tags }
+        return { title, body, tag, tags, handleKeyDown, tags, handleSubmit }
     }
 }
 </script>
